2bafdc84cceb379515e0d2002628362f88c44d9a
Oscar Dominguez <dominguez.celada@gmail.com>
2024-06-11 18:52:48 +0200

    feat: initial version

---
+++ script.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * remove 'release.branches' from Semantic Release (https://github.com/semantic-release/semantic-release)  configuration in package.json
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {}
